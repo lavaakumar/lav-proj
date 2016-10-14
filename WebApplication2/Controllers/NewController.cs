a31b92ed503eb9a58a2550c5c41ba3e6a0159987
lavak <lavak@DESKTOP-LOJ3SCR>
2016-10-13 23:37:28 -0400

    Add project files.

---
+++ WebApplication2/Controllers/NewController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("NewRoute")]
+    public class NewController : ApiController
+    {
+        //public class GetAll
+        //{
+        //    public string FirstName { get; set; }
+        //    public string SecondName { get; set; }
+        //    public SecondGet SecondGet;
+        //}
+        //public class SecondGet
+        //{
+        //    public string Mobile { get; set; }
+        //    public string EmailId { get; set; }
+        //}
+        public class GetAll
+        {
+            public int CustomerId { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Date { get; set; }
+        }
+
+        [Route("firstCall")]
+        [HttpPost]
+        public string firstCall(HttpRequestMessage request, [FromBody] GetAll getAll)
+        {
+            return "Data Reached";
+        }
+
+        [Route("getDataForAngularGrid")]
+        [HttpGet]
+        public List<GetAll> getDataForAngularGrid(HttpRequestMessage request)
+       {
+            DataTable dt = new DataTable();
+            List<GetAll> list = new List<GetAll>();
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=D2ProjectCost;Integrated Security=True");
+                con.Open();
+                var query = "select EmployeeID as customerid,EmployeeName as name,'testadr' as address,email,'123456789' as phno,getdate() as date from TS_Employee";
+                SqlCommand com = new SqlCommand(query, con); //creating SqlCommand object  
+                com.CommandType = CommandType.Text;
+                com.ExecuteNonQuery();
+                con.Close();
+                SqlDataAdapter adptr = new SqlDataAdapter(com);
+                adptr.Fill(dt);
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    GetAll GetAll = new GetAll();
+                    GetAll.CustomerId = Convert.ToInt32(dt.Rows[i]["customerid"]);
+                    GetAll.Name = dt.Rows[i]["name"].ToString();
+                    GetAll.Address = dt.Rows[i]["address"].ToString();
+                    GetAll.Email = dt.Rows[i]["email"].ToString();
+                    GetAll.PhoneNumber = dt.Rows[i]["phno"].ToString();
+                    GetAll.Date = dt.Rows[i]["date"].ToString();
+                    list.Add(GetAll);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            return list;
+        }
+    }
+}
---
+++ WebApplication2/JavaScript1.js
@@ -0,0 +1,44 @@
+﻿var TestCtrl = function ($scope, $http) {
+    $scope.firstCall = function () {
+        debugger;
+        $http({
+            url: "NewRoute/getDataForAngularGrid",
+            dataType: 'json',
+            method: 'GET',
+            data: '',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).success(function (response) {
+            debugger;
+            $scope.CustomerList = response;
+        })
+           .error(function (error) {
+               alert(error);
+           });
+    }
+
+    $scope.SendData = function (Data) {
+        var GetAll = new Object();
+        GetAll.FirstName = Data.firstName;
+        GetAll.SecondName = Data.lastName;
+        GetAll.SecondGet = new Object();
+        GetAll.SecondGet.Mobile = Data.mobile;
+        GetAll.SecondGet.EmailId = Data.email;
+        $scope.IsToBeShown = true;
+        $http({
+            url: "NewRoute/firstCall",
+            dataType: 'json',
+            method: 'POST',
+            data: GetAll,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).success(function (response) {
+            $scope.value = response;
+        })
+           .error(function (error) {
+               alert(error);
+           });
+    }
+};
